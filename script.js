8cf1c4f0f5acc3001f9fda386c03632d869f466a
Exocet-1 <205508512+Exocet-1@users.noreply.github.com>
2025-04-04 01:39:34 -0300

    Added paint on hover.
    Added mouseover event listener on Grid div to listen to all cells' mouseover events. Added a x,y style id to every generated cell because why not.

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const grid = document.getElementById('grid');
 const sliderInput = document.getElementById('slider');
 const sizeSpan = document.getElementById('size-text');
 
-sliderInput.addEventListener('input', () => {
-    sizeSpan.textContent = sliderInput.value;
-    generateGrid(sliderInput.value);
-});
+sliderInput.addEventListener('input', gridUpdate);
+grid.addEventListener('mouseover', paintSquareOnHover);
+
 
 function generateGrid(gridSize) {
     grid.replaceChildren();
@@ -15,8 +14,21 @@ function generateGrid(gridSize) {
         for (let x = 0; x < gridSize; x++) {
             let cell = document.createElement('cell');
             cell.className = 'cell';
+            cell.id = x + ',' + y;
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
 }
+
+function paintSquareOnHover(event){
+    if (event.target.className == 'cell') {
+        event.target.style.backgroundColor = 'black';
+    }
+}
+
+function gridUpdate() {
+    sizeSpan.textContent = sliderInput.value;
+    generateGrid(sliderInput.value);
+}
+gridUpdate();
